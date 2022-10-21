6dcb6ac1d9792ca5306bb6935c7abfa290d99819
LeonardoC <95712634+LeonardoCoaquira@users.noreply.github.com>
2022-10-21 09:43:12 -0500

    av9.1

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,17 +1,13 @@
-<?php 
-$contrasena = "4VPFRk6Ejc";
-$usuario = "sql10528134";
-$nombre_bd = "sql10528134";
+<?php
+date_default_timezone_set('America/Mexico_City');
 
-try {
-	$conexion = new PDO (
-		'mysql:host=sql10.freemysqlhosting.net;
-		dbname='.$nombre_bd,
-		$usuario,
-		$contrasena,
-		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-	);
-} catch (Exception $e) {
-	echo "Problema con la conexion: ".$e->getMessage();
+$conexion = mysqli_connect("sql10.freemysqlhosting.net", "sql10528134", "4VPFRk6Ejc","sql10528134");
+
+if (mysqli_connect_errno()) {
+	printf("Falló la conexión: %s\n", mysqli_connect_error());
+	exit();
 }
+
+mysqli_set_charset($conexion, 'utf8');
+
 ?>
