c9c6b59e25dfc7eadc24b471ef9d9b0e583fd57d
LeonardoC <95712634+LeonardoCoaquira@users.noreply.github.com>
2022-10-28 09:19:54 -0500

    av19.1

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
 
-$conexion = mysqli_connect("sql10.freemysqlhosting.net", "sql10528134", "4VPFRk6Ejc","sql10528134");
+$conexion = mysqli_connect("sql10.freemysqlhosting.net", "sql10529982", "pNqYRUKxzs","sql10529982");
 
 if (mysqli_connect_errno()) {
 	printf("Falló la conexión: %s\n", mysqli_connect_error());
