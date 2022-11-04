18f04d468fa8b8134c22bced218145d8b7bf372f
SandraHuancara <95718206+SandraHuancara@users.noreply.github.com>
2022-11-04 16:48:01 -0500

    Update schoolperiods.js

--- js/modules/schoolperiods.js
+++ js/modules/schoolperiods.js
@@ -1,10 +1,3 @@
-/*-------------------------------------------
-  schoolperiods.js
-  By Diego Carmona Bernal - CBDX
-  www.diegocarmonabernal.com
-  www.mysoftup.com
--------------------------------------------*/
-
 $('.select').select2({
     minimumResultsForSearch: Infinity
 });
