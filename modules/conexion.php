f56bc126b7411b0831dd71f200cd92c17b32ec7e
LeonardoC <95712634+LeonardoCoaquira@users.noreply.github.com>
2022-11-03 20:20:50 -0500

    newCredential

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
 
-$conexion = mysqli_connect("sql10.freemysqlhosting.net", "sql10529982", "pNqYRUKxzs","sql10529982");
+$conexion = mysqli_connect("sql9.freemysqlhosting.net", "sql9539971", "Ya4HLB3ifU","sql9539971");
 
 if (mysqli_connect_errno()) {
 	printf("Falló la conexión: %s\n", mysqli_connect_error());
